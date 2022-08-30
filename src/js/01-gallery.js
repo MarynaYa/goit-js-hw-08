372c115abd193fe70ef33b6bbc8c575450e0b0a7
MarynaYa <yamarina30@gmail.com>
2022-08-30 15:21:46 +0300

    task1-commit

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,27 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+const galleryItemsAll = document.querySelector(`.gallery`);
+console.log(galleryItemsAll);
+
+function createImageCards(array) {
+    return array
+      .map(
+        ({ preview, original, description }) =>
+        `<a class="gallery__item" href=${original}>
+    <img class="gallery__image" src=${preview} alt="${description}" />
+  </a>`, 
+      )
+      .join('');
+  }
+  
+const cardsMarkup = createImageCards(galleryItems);
+galleryItemsAll.insertAdjacentHTML(`afterbegin`, cardsMarkup);
+
+const lightbox = new SimpleLightbox('.gallery a', {   
+    captionsData: 'alt',
+    captionDelay: 250,
+  });
