f2c4fc7cd2a83b8142127e171285dc3de569cd27
MarynaYa <yamarina30@gmail.com>
2022-08-30 15:57:11 +0300

    task2-commit

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,22 @@
+import Player from '@vimeo/player';
 
+// задержка
+import throttle from 'lodash.throttle';
+
+
+const iframe = document.querySelector('#vimeo-player');
+// создаем копию
+const player = new Player(iframe);
+
+// записываем текущее значение времени
+function onPlay(event) {
+    localStorage.setItem('videoplayer-current-time', event.seconds);
+  };
+// получаем локальное xранилища текущую позицию
+const currentTimeOfVideo = localStorage.getItem('videoplayer-current-time');
+
+// если есть сохраненная текущая позиция и она отлична от 0, устанавливаем на плеере сохраненную текущую позицию
+currentTimeOfVideo ? player.setCurrentTime(currentTimeOfVideo) : null;
+
+// при запуске просмотра вызываем обработку и передаем текущие секунды в onPlay с интервалом в 1 секунду
+player.on('timeupdate', throttle(onPlay, 1000));
