994e98a4913cace94810e88cdf97f3ffc673a995
MarynaYa <yamarina30@gmail.com>
2022-08-31 20:21:11 +0300

    dz3-commit

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -8,15 +8,17 @@ const iframe = document.querySelector('#vimeo-player');
 // создаем копию
 const player = new Player(iframe);
 
+// при запуске просмотра вызываем обработку и передаем текущие секунды в onPlay с интервалом в 1 секунду
+player.on('timeupdate', throttle(onPlay, 1000));
+
 // записываем текущее значение времени
 function onPlay(event) {
     localStorage.setItem('videoplayer-current-time', event.seconds);
   };
-// получаем локальное xранилища текущую позицию
-const currentTimeOfVideo = localStorage.getItem('videoplayer-current-time');
+
+
+// установим текущее позицию хранилища
+const currentTime = localStorage.getItem('videoplayer-current-time');
 
 // если есть сохраненная текущая позиция и она отлична от 0, устанавливаем на плеере сохраненную текущую позицию
-currentTimeOfVideo ? player.setCurrentTime(currentTimeOfVideo) : null;
-
-// при запуске просмотра вызываем обработку и передаем текущие секунды в onPlay с интервалом в 1 секунду
-player.on('timeupdate', throttle(onPlay, 1000));
+currentTime ? player.setCurrentTime(currentTime) : null;
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,41 @@
+import throttle from 'lodash.throttle';
 
+// ключ к хранилищу
+const STORAGE_KEY = 'feedback-form-state';
+
+// получаем доступ к элементам формы
+const refs = {
+  form: document.querySelector('.feedback-form'),
+  textarea: document.querySelector('.feedback-form  textarea'),
+  input: document.querySelector('.feedback-form  input'),
+};
+// добавляемые значения
+let formData = {};
+// добавляем ф-нал 
+onFormState();
+refs.form.addEventListener('submit', onFormSubmit);
+refs.form.addEventListener('input', throttle(onTextareaInput, 500));
+
+// Ф-ция сохранения текущих значений (1)останавливаем по умолчанию поведение; 2)получаем значение ввода; 3) сохраняем значание в хранилище)
+function onTextareaInput(event) {
+    event.preventDefault();
+    formData[event.target.name] = event.target.value;  
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}
+// ф-ция проверки состояния хранилища (1) получаем значение хранилища; 2) если там есть даннные заполняем ими поля, в противном случем пустая строка
+function onFormState() {
+    const messageStoregeSave = localStorage.getItem(STORAGE_KEY);
+    const parseStorageMessage = JSON.parse(messageStoregeSave);
+
+    if (messageStoregeSave) {
+        (refs.textarea.value = parseStorageMessage.message || "");
+        (refs.input.value = parseStorageMessage.email || "");
+    }
+}
+
+// очищает Сабмит (1) останавливаем по умолчанию поведение; 2)очищаем форму; 3) очищаем хранилище)
+function onFormSubmit(event) {
+    event.preventDefault();
+    event.currentTarget.reset();
+    localStorage.removeItem(STORAGE_KEY);
+}
