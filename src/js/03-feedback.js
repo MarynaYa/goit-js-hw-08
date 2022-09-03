dabc46d49e870ba95c7f69a633450a1f0b7ba1b5
MarynaYa <yamarina30@gmail.com>
2022-09-03 11:28:52 +0300

    onForm-commit

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -18,22 +18,24 @@ refs.form.addEventListener('input', throttle(onTextareaInput, 500));
 
 // Ф-ция сохранения текущих значений (1)останавливаем по умолчанию поведение; 2)получаем значение ввода; 3) сохраняем значание в хранилище)
 function onTextareaInput(event) {
-    //event.preventDefault();
     formData[event.target.name] = event.target.value;  
     localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
 }
 // ф-ция проверки состояния хранилища (1) получаем значение хранилища; 2) если там есть даннные заполняем ими поля, в противном случем пустая строка
 function onFormState() {
     let messageStoregeSave = localStorage.getItem(STORAGE_KEY);
-  
-  if (messageStoregeSave) {
+     // парсим сообщение из хранилища, если в хранилище что-то есть
+       if (messageStoregeSave) {
     messageStoregeSave = JSON.parse(messageStoregeSave);
-   // parseStorageMessage = formData;
+        Object.entries(messageStoregeSave).forEach(([name, value]) => {
+            formData[name] = value;
+            refs.form.elements[name].value = value;
+        })
   }
-    if (messageStoregeSave) {
-        (refs.textarea.value = messageStoregeSave.message || "");
-        (refs.input.value = messageStoregeSave.email || "");
-    }  
+    //if (messageStoregeSave) {
+     //(refs.textarea.value = messageStoregeSave.message || "");
+       //(refs.input.value = messageStoregeSave.email || "");
+   //}  
 }
 // очищает Сабмит (1) останавливаем по умолчанию поведение; 2)очищаем форму; 3) очищаем хранилище)
 function onFormSubmit(event) {
